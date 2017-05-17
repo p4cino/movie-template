4ac49030667bcd33073b97973535599b3b5cda20
p4cino <wojtexdesign@hotmail.com>
2017-05-17 11:58:37 +0200

    niemal finallne poprawki

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,3 +87,59 @@ $(".flex-container")
     .mouseleave(function () {
         $('.button-overlay').hide();
     });
+
+//=====Scale Video=====
+
+$(document).ready(function () {
+    scaleVideoContainer();
+    initBannerVideoSize('.video__container .poster img');
+    initBannerVideoSize('.video__container video');
+    $(window).on('resize', function () {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video__container .poster img');
+        scaleBannerVideoSize('.video__container video');
+    });
+});
+
+function scaleVideoContainer() {
+
+    var height = $(window).height();
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height', unitHeight);
+
+}
+
+function initBannerVideoSize(element) {
+
+    $(element).each(function () {
+            $(this).data('height', $(this).height());
+            $(this).data('width', $(this).width());
+        }
+    );
+    scaleBannerVideoSize(element);
+}
+
+function scaleBannerVideoSize(element) {
+    var windowWidth = $(window).width(),
+        windowHeight = $(window).height(),
+        videoWidth,
+        videoHeight;
+    $(element).each(function () {
+            var videoAspectRatio = $(this).data('height') / $(this).data('width'),
+                windowAspectRatio = windowHeight / windowWidth;
+            if (videoAspectRatio > windowAspectRatio) {
+                videoWidth = windowWidth;
+                videoHeight = videoWidth * videoAspectRatio;
+                $(this).css({'top': -(videoHeight - windowHeight) / 2 + 'px', 'margin-left': 0});
+            } else {
+                videoHeight = windowHeight;
+                videoWidth = videoHeight / videoAspectRatio;
+                $(this).css({'margin-top': 0, 'margin-left': -(videoWidth - windowWidth) / 2 + 'px'});
+            }
+            $(this).width(videoWidth).height(videoHeight);
+            $('.video__container video').addClass('fadeIn animated');
+
+
+        }
+    );
+}
